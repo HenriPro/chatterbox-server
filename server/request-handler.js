dfac2de8752ed97e7ebab294ffe5411d3ba12cd5
Hack Reactor Student <student@hackreactor.com>
2017-06-08 17:04:34 -0700

    parses URL for pathname. Renders index.html to local host.

--- server/request-handler.js
+++ server/request-handler.js
@@ -1,18 +1,7 @@
-var fs = require('fs');
+const fs = require('fs');
+const url = require('url');
+const path = require('path');
 
-/*************************************************************
-
-You should implement your request handler function in this file.
-
-requestHandler is already getting passed to http.createServer()
-in basic-server.js, but it won't work as is.
-
-You'll have to figure out a way to export this function from
-this file and include it in basic-server.js so that it actually works.
-
-*Hint* Check out the node module documentation at http://nodejs.org/api/modules.html.
-
-**************************************************************/
 var allPreviousMessages = [
   {
     username: 'Jono',
@@ -27,21 +16,8 @@ var endResponse = function(response, statusCode, headers, responseStr) {
 };
 
 
-var requestHandler = function(request, response) {
-  // Request and Response come from node's http module.
-  //
-  // They include information about both the incoming request, such as
-  // headers and URL, and about the outgoing response, such as its status
-  // and content.
-  //
-  // Documentation for both request and response can be found in the HTTP section at
-  // http://nodejs.org/documentation/api/
 
-  // Do some basic logging.
-  //
-  // Adding more logging to your server can be an easy way to get passive
-  // debugging help, but you should always be careful about leaving stray
-  // console.logs in your code.
+var requestHandler = function(request, response) { 
   console.log('Serving request type ' + request.method + ' for url ' + request.url);
   var headers = {
     'access-control-allow-origin': '*',
@@ -58,35 +34,30 @@ var requestHandler = function(request, response) {
     'access-control-max-age': 10, // Seconds.
     'Content-Type': 'text/html'
   };
-  // check the type of request method
-  // The outgoing status.
+
   var statusCode = 200;
   var responseObj = {results: []};
+  var pathName = url.parse(request.url).pathname;
   
-  
-  if (request.method === 'GET') {
-    if (request.url === '/classes/messages?order=-createdAt' || request.url === '/classes/messages') {
+
+  var apiCall = function() {
+    if (request.method === 'GET') {
       responseObj.results = allPreviousMessages;
       endResponse(response, statusCode, headers, JSON.stringify(responseObj));
-    } else if ( request.url === '/index.html' ) {
-      var htmlResponseObj = '<h1>Hello World!</h1>';     
-      endResponse(response, statusCode, htmlHeaders, htmlResponseObj);
-    } else { // do URL parsing
-      statusCode = 404;    
-      endResponse(response, statusCode, headers, JSON.stringify(responseObj));
-    }
-    
-  } else if (request.method === 'POST') {
-    if (request.url === '/classes/messages') {
+
+    } else if (request.method === 'POST') {
       var currentMessage = [];
+
       request.on('error', (err) => {
         console.log('error', err);
       });
+
       request.on('data', (chunk) => {
         if (Buffer.isBuffer(chunk)) {
           currentMessage.push(chunk);
         }
       });
+
       request.on('end', () => {
         if (currentMessage.length > 0) {
           var newMessage = JSON.parse(Buffer.concat(currentMessage).toString('utf-8'));
@@ -96,12 +67,33 @@ var requestHandler = function(request, response) {
         statusCode = 201;
         endResponse(response, statusCode, headers, JSON.stringify(responseObj));
       });
-    }
-  } else if (request.method === 'OPTIONS') {
-    endResponse(response, statusCode, headers, JSON.stringify(responseObj));
+    } else if (request.method === 'OPTIONS') {
+      endResponse(response, statusCode, headers, JSON.stringify(responseObj));
+    } 
+  };
+  
+  var htmlCall = function(pathname) {
+    var htmlResponseObj = '<h1>Hello World!</h1>'; 
+    // console.log(__dirname)
+    fs.readFile('./client' + pathname, (err, data) => {
+      if (err) { 
+        console.log(err);
+        endResponse(response, 404, htmlHeaders, '<h1>ERROR 404</h1>'); 
+      } else {
+        endResponse(response, 200, htmlHeaders, data); 
+      }
+      console.log(data);
+    });
+
+    ///Users/student/code/hrsf78-chatterbox-server/server
+    // endResponse(response, statusCode, htmlHeaders, htmlResponseObj);
+  };
+
+  if (pathName === '/classes/messages') {
+    apiCall();
+  } else {
+    htmlCall(pathName);
   }
-
-
 
 };
 
